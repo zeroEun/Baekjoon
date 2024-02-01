d64c3a9f212db8516da765b912c3c9c7bfb681b8
YeongEun <85044953+zeroEun@users.noreply.github.com>
2024-02-01 18:54:42 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+        String inputText = br.readLine();
+
+        System.out.println("1");
+        System.out.println("0");
+
+        br.close();
+    }
+}
