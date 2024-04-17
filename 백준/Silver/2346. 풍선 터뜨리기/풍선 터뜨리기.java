27e8633c7e8b1722cdc3c65b0d8e03964377389d
YeongEun <85044953+zeroEun@users.noreply.github.com>
2024-04-17 19:29:29 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 188 ms, Memory: 17392 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(reader);
+
+        int cnt = Integer.parseInt(br.readLine());
+        String[] readList = br.readLine().split(" ");
+
+        br.close();
+
+        ArrayDeque<Map> deque = new ArrayDeque<>();
+        StringJoiner stringJoiner = new StringJoiner(" ");
+
+        for (int i = 0; i < readList.length; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            map.put("seq", i + 1);
+            map.put("num", Integer.parseInt(readList[i]));
+            deque.add(map);
+        }
+
+        Map poll = deque.pollFirst();
+        stringJoiner.add(String.valueOf(poll.get("seq")));
+        int num = (int) poll.get("num");
+
+        while (!deque.isEmpty()) {
+
+            if (num < 0) {
+                for (int i = 1; i < Math.abs(num); i++){
+                    deque.addFirst(deque.pollLast());
+                }
+                poll = deque.pollLast();
+            }
+
+            if (num > 0) {
+
+                for (int i = 1; i < Math.abs(num); i++){
+                    deque.addLast(deque.pollFirst());
+                }
+                poll = deque.pollFirst();
+            }
+
+            stringJoiner.add(String.valueOf(poll.get("seq")));
+            num = (int) poll.get("num");
+
+        }
+
+        System.out.println(stringJoiner);
+    }
+}
